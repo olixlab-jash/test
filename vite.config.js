768512256430fce59833bc1fddee611146484212
Bhavy Mavani <bhavymavani@Mac.lan>
2025-06-24 11:20:04 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allows access from external IP/domain
+    allowedHosts: ['oceyon.ch'] // allows oceyon.ch as a valid host
+  }
 })
